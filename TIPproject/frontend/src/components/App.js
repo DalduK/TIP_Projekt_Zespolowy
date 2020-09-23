dd51cf6c493050b68cf778eaf864107246528eab
flopczak <jakubflorczak27@gmail.com>
2020-09-23 19:32:00 +0200

    add Register, Login, Navbar components

--- TIPproject/frontend/src/components/App.js
+++ TIPproject/frontend/src/components/App.js
@@ -1,11 +1,37 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 
+import Register from "./accounts/Register";
+import Login from "./accounts/Login";
+import UnNavbar from "./layout/UnNavbar";
 import { Provider } from "react-redux";
+import store from "../store";
+import PrivateRoute from "./common/PrivateRoute";
+
+
 class App extends Component {
   render() {
-    return <div>dd</div>;
+    return (
+        <Provider store={store}>
+        <Router>
+          <Fragment>
+            <UnNavbar />
+            <div className="container">
+              <Switch>
+                <PrivateRoute exact path="/" component={() => {"test"}} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </Provider>
+    )
   }
 }
 
