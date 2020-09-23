cff9aeb780e05c786affcdfb4b1be220b0567261
flopczak <jakubflorczak27@gmail.com>
2020-09-23 18:54:48 +0200

    Authorization

---
+++ TIPproject/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router } from "react-router-dom";
+
+import { Provider } from "react-redux";
+class App extends Component {
+  render() {
+    return <div>dd</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
